e939799c6c8eb290ebcd05efb64b9bbe3862fea3
vidhyabathi-002 <kk3777096@gmail.com>
2026-05-07 13:11:00 +0530

    Add bank IFSC helper functions

--- opps day 17/oops.py
+++ opps day 17/oops.py
@@ -3,8 +3,18 @@ bankname='sbi'
 branch='chennai'        
 ifsc=123456789
 #
+def bank_ifsc_correct():
+    print("account is correct branch dtected")
+
+def bank_ifsc_incorrect():
+    print("account is incorrect branch not detected")
 
 #bank 02
 bankname='hdfc'
 branch='chennai'
 ifsc=987654321
+
+def bank_ifsc_correct():
+    print("account is correct branch dtected")
+def bank_ifsc_incorrect():
+    print("account is incorrect branch not detected")
